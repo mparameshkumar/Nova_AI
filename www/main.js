fd040392acac681ddd147c3e7dcd30110b51950d
mparameshkumar <116893249+mparameshkumar@users.noreply.github.com>
2024-09-22 22:09:42 +0530

    Day2_B

--- www/main.js
+++ www/main.js
@@ -9,4 +9,15 @@ $(document).ready(function () {
             effect: 'bounceOut'
         }
     });
+
+    //Siri Configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 1000,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true,
+      });
 });
